f481280a8e16023afb98a59611f9f8e2190b99ca
yawar1101 <mohdyawars@gmail.com>
2023-11-05 08:22:37 +0530

    add axios to script

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// const btn = document.querySelector('button');
-
-// console.log(btn);
-
-// btn.addEventListener('submit', (e) => {
-
-//     console.log('COMING');
-//     e.preventDefault();
-
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const phone = document.getElementById('phome').value;
-
-//     const userDetails = {
-//         name: name,
-//         email: email,
-//         phone: phone,
-//     };
-
-//     localStorage.setItem('userDetails', JSON.stringify(userDetails));
-
-//     alert('Details have been saved to local storage');
-// });
-
 const userForm = document.getElementById('userForm');
 const userDataDiv = document.getElementById('user-data');
 
@@ -52,7 +28,15 @@ userForm.addEventListener('submit', (event) => {
             email: email,
             phone: phone
         };
-        localStorage.setItem(userKey, JSON.stringify(userDetails));
+
+        axios.post(
+            'https://crudcrud.com/api/ba25bf77b5054d929d3182e23dc182b0/appointments/', userDetails
+        )
+            .then(response => console.log(response))
+            .catch(err => console.log(console.log(err)))
+
+        // localStorage.setItem(userKey, JSON.stringify(userDetails));
+
     }
 
     displayUserData();
@@ -111,49 +95,4 @@ userForm.addEventListener('submit', (event) => {
         }
     }
 
-    // Initial call to display user data when the page loads
     displayUserData();
-
-
-//     const timestamp = new Date().getTime();
-//     const userKey = `userDetails_${timestamp}`;
-
-//     const userDetails = {
-//         name: name,
-//         email: email,
-//         phone: phone,
-//     };
-
-//     localStorage.setItem(userKey, JSON.stringify(userDetails));
-
-//     const userContainer = document.createElement('div');
-//     userContainer.classList.add('user-container');
-
-//     const nameParagraph = document.createElement('p');
-//     nameParagraph.textContent = `Name: ${userDetails.name}`;
-
-//     const emailParagraph = document.createElement('p');
-//     emailParagraph.textContent = `Email: ${userDetails.email}`;
-
-//     const ageParagraph = document.createElement('p');
-//     ageParagraph.textContent = `Age: ${userDetails.age}`;
-
-//     const deleteButton = document.createElement('button');
-//     deleteButton.textContent = 'Delete';
-//     deleteButton.classList.add('btn', 'btn-danger', 'delete-button');
-
-//     deleteButton.addEventListener('click', () => {
-//         localStorage.removeItem(userKey);
-
-//         userDataDiv.removeChild(userContainer);
-//     });
-
-//     userContainer.appendChild(nameParagraph);
-//     userContainer.appendChild(emailParagraph);
-//     userContainer.appendChild(ageParagraph);
-//     userContainer.appendChild(deleteButton);
-
-//     userDataDiv.appendChild(userContainer);
-
-//     alert('User details have been saved in local storage.');
-// });
